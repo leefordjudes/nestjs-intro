187e11a2413026ba14bcd215a68bb3b44444b500
leefordjudes <leefordjudes@gmail.com>
2020-02-21 21:58:50 +0530

    initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,53 @@
+import { Controller, Post, Body, Get, Param, Patch, Delete, Put } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productService: ProductsService){}
+
+  @Post()
+  async addProduct(
+  // @Body() completeBody: {title: string, desc: string, price: number},
+  @Body('title') prodTitle: string, 
+  @Body('desc') prodDesc: string,
+  @Body('price') prodPrice: number,) {
+    const newProductId = await this.productService.insertProduct(prodTitle,prodDesc,prodPrice);
+    return { id: newProductId }
+  }
+
+  @Get()
+  async getAllProducts(){
+    const products = await this.productService.getAllProducts();
+    //return products;
+    return products.map((prod)=>({id: prod.id, title: prod.title, description: prod.description, price: prod.price,}));
+  }
+
+  @Get(':id')
+  async getProduct(@Param('id') prodId: string){
+    const prod = await this.productService.getProduct(prodId);
+    return {id: prod.id, title: prod.title, description: prod.description, price: prod.price};
+  }
+
+  @Patch(':id')// patch product will update the given values only. ie) PATCH applies a partial update to the resource.
+  async updateProduct(@Param('id') prodId: string,  @Body('title') prodTitle: string, 
+    @Body('description') prodDesc: string, @Body('price') prodPrice: number)  {
+      const prod = await this.productService.updateProduct(prodId, prodTitle,prodDesc,prodPrice);
+      return {id: prod.id, title: prod.title, description: prod.description, price: prod.price};
+  }
+  @Delete(':id')
+  async deleteProduct(@Param('id') prodId: string){
+    const deletedcount = await this.productService.deleteProduct(prodId);
+    return (deletedcount===1)?'Object deleted!':'Object not deleted!';
+  }
+/*
+  @Put(':id')// PUT overwrites the entire entity if it already exists, and creates a new resource if it doesn’t exist.
+  editProduct(@Param('id') id: string,  @Body() completeBody: {title: string, description: string, price: number})  {
+      return this.productService.editProduct(id, completeBody.title,completeBody.description,completeBody.price);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string){
+    this.productService.deleteProduct(prodId);
+  }
+  */
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,114 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as uuid from 'uuid/v4';
+import { Product } from './product.model';
+
+@Injectable()
+export class ProductsService {
+  private products: Product[] = [];
+  constructor(@InjectModel('Product') private readonly productModel: Model<Product>){}
+
+  async insertProduct(title: string, desc: string, price: number) {
+    //const newProdId = uuid();
+    //const newProduct = new Product(newProdId,title,desc,price);
+    const newProduct = new this.productModel({title, description: desc, price,});  
+    //this.products.push(newProduct);
+    const result = await newProduct.save();
+    console.log(result);
+    //return newProdId;
+    return result.id as string;
+  }
+
+  async getAllProducts() {
+    const products = await this.productModel.find().exec();
+    //return products;//
+    return products.map((prod)=>({id: prod.id, title: prod.title, description: prod.description, price: prod.price}));
+    //return [...this.products]; //returns all products as newly created array
+  }
+
+  async getProduct(prodId: string){
+  //  const product = this.findProduct(prodId)[0];
+  //  return { ...product }; //returns a single product as newly created product.
+    return await this.findOnlineProduct(prodId); 
+  }
+  async updateProduct(prodId: string, title: string, desc: string, price: number){
+    const updatedProduct = await this.findOnlineProduct(prodId);
+    
+    if(title){
+      updatedProduct.title = title;
+    }
+    if(desc){
+      updatedProduct.description = desc;
+    }
+    if(price){
+      updatedProduct.price = price;
+    }
+    updatedProduct.save();
+
+    return await this.getProduct(prodId);
+  }
+
+  async deleteProduct(prodId: string){
+    try{
+      const deleted = await this.productModel.deleteOne({_id:prodId}).exec();
+      return deleted.deletedCount;
+    } catch(err){
+      throw new NotFoundException('Could not find product.');
+    }
+  }
+
+/*
+  updateProduct(prodId: string, title: string, desc: string, price: number){
+    const [product, index] = this.findProduct(prodId);
+    const updatedProduct = {...product};
+    if(title){
+      updatedProduct.title = title;
+    }
+    if(desc){
+      updatedProduct.description = desc;
+    }
+    if(price){
+      updatedProduct.price = price;
+    }
+    this.products[index] = updatedProduct;
+
+    return {...this.products[index]};
+  }
+
+  editProduct(id: string, title: string, description: string, price: number){
+    const index = this.findProduct(id)[1];
+    const product = {id,title,description,price};
+    this.products[index]={...product};
+    
+    return {...this.products[index]};
+  }
+
+  deleteProduct(prodId: string){
+    const index = this.findProduct(prodId)[1];
+    this.products.splice(index,1);
+  }
+
+  private findProduct(id: string): [Product, number] {
+    const productIndex = this.products.findIndex((prod) => prod.id === id);
+    const product = this.products[productIndex];
+    if(!product){
+      throw new NotFoundException('Could not find product.');
+    }
+    return [product, productIndex];
+  }
+*/
+  private async findOnlineProduct(id: string): Promise<Product> {
+    let product;
+    try{
+      product = await this.productModel.findById(id);
+    } catch(err){
+      throw new NotFoundException('Could not find product.');
+    }
+    if(!product){
+      throw new NotFoundException('Could not find product.');
+    }
+    return product;
+    //return {id: product.id, title: product.title, description: product.description, price: product.price};
+  }
+}
